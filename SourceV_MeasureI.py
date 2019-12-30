16f647ba74a77174ce44fcf1691b6d1efbfb71d5
Vanguo996 <guowang939@gmail.com>
2019-12-30 21:52:50 +0800

    modify volt

--- SourceV_MeasureI.py
+++ SourceV_MeasureI.py
@@ -24,9 +24,9 @@ def initial_csv_62():
         csv_writer.writeheader()
     return filename6
 
-def pluse_2400_self(high_curr1 = -100e-9):
+def pluse_2400_self(high_curr1 = 5e-3):
     
-    i=7
+    i=30
     high_curr = str(high_curr1)
     temp2 = 0
     temp1 = 0
@@ -36,14 +36,11 @@ def pluse_2400_self(high_curr1 = -100e-9):
     count1 = 0
     instr24.write(':outp on')
     while i:
-
-
-        j = temp2 + 40  #设置间隔大小
-
+        j = temp2 + 40
         start_low = time.perf_counter()
         while j:
-           
-            instr24.write('sour:curr:lev 0.0000000')
+            
+            instr24.write('sour:volt:lev 0.0000000')
             # instr24.write('init')
             temp4=instr24.query(':read?').split(',')
             temp4[2] = str(0)
@@ -54,22 +51,18 @@ def pluse_2400_self(high_curr1 = -100e-9):
         count +=1
         stop_low = time.perf_counter()
         print(f'low time is {stop_low-start_low}')
-
-        #temp2 += rate  #取消递增
-
+        # temp2 += rate
         print(count)
         if count == 1:
             rate +=0
-
-        #rate += 40   #周期的增加的速率 默认5s 
+        rate +=40
         
 
-        w = temp1 + 40  #设置间隔大小
-
+        w = temp1 + 40
         start_low1 = time.perf_counter()
         while w:
             
-            instr24.write('sour:curr:lev %s ' % high_curr)
+            instr24.write('sour:volt:lev %s ' % high_curr)
             # instr24.write('init')
             temp = instr24.query(':read?').split(',')
             resistance = float(temp[0])/float(temp[1])
@@ -81,13 +74,11 @@ def pluse_2400_self(high_curr1 = -100e-9):
         count1 +=1
         stop_low1 = time.perf_counter()
         print(f'high time is {stop_low1-start_low1}')
-
-        #temp1 += rate1 # 取消递增
-
+        
+        # temp1 += rate1
         if count1==1:
             rate1 += 0
-
-        #rate1 +=40     #改变速率  默认5s
+        rate1 +=40
 
         i -=1
         print(i)
@@ -99,8 +90,9 @@ def plot_24():
     global ax2
     global ax3
     fig = plt.figure()
-    ax1 = fig.add_subplot(211)
-    ax2 = fig.add_subplot(212)
+    ax1 = fig.add_subplot(311)
+    ax2 = fig.add_subplot(312)
+    ax3 = fig.add_subplot(313)
     ani = FuncAnimation(fig,animate,interval=100)
     plt.show()
 
@@ -109,24 +101,26 @@ def animate(i):
     x = data['timestamp']
     c = data['current']
     r = data['res']
+    v = data['voltage']
     ax1.cla()
     ax2.cla()
-    
+    ax3.cla()
     ax1.plot(x,c,marker='o',label='time-current',color='b')
     ax2.plot(x,r,marker='*',label='time-resistance',color='r')
-
+    ax3.plot(x,v,marker='o',label='time-volt')
     ax1.legend()
     ax2.legend()
+    ax3.legend()
 
 if __name__ == "__main__":
     ax1 = None
     ax2 = None
     ax3 = None
-   
+    
     instr24.write('*rst')
-    instr24.write(':sour:func curr')
-    instr24.write(':sens:volt:prot 200')
-    instr24.write(':sens:func "volt"')
+    instr24.write(':sour:func volt')
+    instr24.write(':sens:curr:prot 1')
+    instr24.write(':sens:func "curr"')
     instr24.write(':aver:coun 1')
     instr24.write(':sour:del 0')
     
